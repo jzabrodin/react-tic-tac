ae7d19307ac0eb573e459a8ed068f5617af63707
yevgen <jzabrodin@gmail.com>
2020-01-05 13:52:46 +0200

    * rows and cols renders by for

--- src/index.js
+++ src/index.js
@@ -27,31 +27,19 @@ class Board extends React.Component {
         const col_count = this.props.size[1];
         let counter = 0;
 
-        return (<div>
+        let rows = [];
 
-        </div>);
+        for (let i = 0; i < row_count; i++) {
+            let cols = [];
+            for (let j = 0; j < col_count; j++) {
+                cols.push(this.renderSquare(counter));
+                counter++;
+            }
+            rows.push(<div className="board-row">{cols}</div>);
+        }
 
+        return rows;
 
-        return (
-
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
     }
 }
 
